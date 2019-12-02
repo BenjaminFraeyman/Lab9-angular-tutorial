49eef53e0beb3d6b5995569b4ebc3fffd297ce46
BenjaminFraeyman <benjaminfraeyman@gmail.com>
2019-12-02 10:09:35 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { ProductAlertComponent } from './product-alert/product-alert.component';
 
 @NgModule({
   imports: [
